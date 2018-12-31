96b099bef736eb77340fd76f291cadabd5889760
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2018-12-30 20:40:50 -0800

    Format with prettier

--- diatoms/diatom-01.js
+++ diatoms/diatom-01.js
@@ -1,140 +1,145 @@
 // several varieties of simple radial patterns
 
-let REPETITIONS = 13;  // how many diatoms to draw
-let SEED;  // seed for RNG
-let diatoms = [];  // to be filled with diatom parameters
+let REPETITIONS = 13; // how many diatoms to draw
+let SEED; // seed for RNG
+let diatoms = []; // to be filled with diatom parameters
 
 let p = new window.URL(document.location).searchParams;
-if (p.get('num') !== null) {
-	let num = parseInt(p.get('num'));
-	if (num != NaN) {
-		REPETITIONS = num;
-		console.debug('Repetitions set from query parameters');
-	}
+if (p.get("num") !== null) {
+  let num = parseInt(p.get("num"));
+  if (num != NaN) {
+    REPETITIONS = num;
+    console.debug("Repetitions set from query parameters");
+  }
 }
-if (p.get('seed') !== null) {
-	SEED = p.get('seed');
+if (p.get("seed") !== null) {
+  SEED = p.get("seed");
 } else {
-	SEED = Math.random().toString();
+  SEED = Math.random().toString();
 }
 Math.seedrandom(SEED);
 console.info(`random seed: ${SEED}`);
 
-function updatePermishlink(id='permishlink') {
-	document.getElementById(id).href = `?num=${REPETITIONS}&seed=${SEED}`;
+function updatePermishlink(id = "permishlink") {
+  document.getElementById(id).href = `?num=${REPETITIONS}&seed=${SEED}`;
 }
-window.addEventListener('load', () => updatePermishlink());
-
+window.addEventListener("load", () => updatePermishlink());
 
 // helpful functions
 function deg2rad(deg) {
-	return deg / 180 * Math.PI;
+  return (deg / 180) * Math.PI;
 }
 
 function sind(deg) {
-	return Math.sin(deg2rad(deg));
+  return Math.sin(deg2rad(deg));
 }
 
 function cosd(deg) {
-	return Math.cos(deg2rad(deg));
+  return Math.cos(deg2rad(deg));
 }
 
 function tand(deg) {
-	return Math.tan(deg2rad(deg));
+  return Math.tan(deg2rad(deg));
 }
 
 // rng for inclusive ranges of ints (Math.random returns between [0,1))
 function random(lower, upper) {
-	return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
-
 
 // prep page
 initCanvas = function(canvas, drawFunc) {
-	// Create an empty project and a view for the canvas:
-	paper.setup(canvas);
-	drawFunc();
-	// Draw the view now:
-	// paper.view.draw();
-}
+  // Create an empty project and a view for the canvas:
+  paper.setup(canvas);
+  drawFunc();
+  // Draw the view now:
+  // paper.view.draw();
+};
 
-window.addEventListener('load', () => {
-	generateCanvases(REPETITIONS);
-	console.log('View diatom info with the output above, or using the `diatoms` global object, e.g. `diatoms[<number>]`.');
-}, false);
-
+window.addEventListener(
+  "load",
+  () => {
+    generateCanvases(REPETITIONS);
+    console.log(
+      "View diatom info with the output above, or using the `diatoms` global object, e.g. `diatoms[<number>]`."
+    );
+  },
+  false
+);
 
 // drawing
-function diatom(point, iter=20, radius1=15, radius2=20) {
-	for (var i=0; i < iter; i++) {
-		angle = (360 / iter) * i;
-		offset = new paper.Point({angle: angle, length: radius1});
-		c = new paper.Path.Circle(point.add(offset), radius2);
-		c.strokeColor = 'black';
-	}
+function diatom(point, iter = 20, radius1 = 15, radius2 = 20) {
+  for (var i = 0; i < iter; i++) {
+    angle = (360 / iter) * i;
+    offset = new paper.Point({ angle: angle, length: radius1 });
+    c = new paper.Path.Circle(point.add(offset), radius2);
+    c.strokeColor = "black";
+  }
 }
 
 function getRandomParams() {
-	let minRad = 2;
-	let maxTotalRad = 40;
-	let iter = random(3, 45);
-	let angle = 360 / iter;
-	let r1 = random(minRad, maxTotalRad/(tand(angle/2)+1));
-	let totalRadLimit = maxTotalRad - r1;
-	let touchLimit = Math.ceil(tand(angle / 2) * r1);
-	let r2 = random(touchLimit, totalRadLimit);
-	return {iter, r1, r2};
+  let minRad = 2;
+  let maxTotalRad = 40;
+  let iter = random(3, 45);
+  let angle = 360 / iter;
+  let r1 = random(minRad, maxTotalRad / (tand(angle / 2) + 1));
+  let totalRadLimit = maxTotalRad - r1;
+  let touchLimit = Math.ceil(tand(angle / 2) * r1);
+  let r2 = random(touchLimit, totalRadLimit);
+  return { iter, r1, r2 };
 }
 
-function diatomFromRepr({iter, r1, r2}) {
-	return (point) => diatom(point, iter, r1, r2);
-};
+function diatomFromRepr({ iter, r1, r2 }) {
+  return point => diatom(point, iter, r1, r2);
+}
 
 function getSvgString(num) {
-	if (num in diatoms) {
-		return diatoms[num].project.exportSVG({asString: true})
-	} else {
-		return new Error(`diatom ${num} does not exist`);
-	}
-};
+  if (num in diatoms) {
+    return diatoms[num].project.exportSVG({ asString: true });
+  } else {
+    return new Error(`diatom ${num} does not exist`);
+  }
+}
 
 function downloadDiatom(params) {
-	// create a diatom from a representation
-	let c = document.createElement('canvas');
-	c.width = 100;
-	c.height = 100;
-	initCanvas(c, () => {
-		diatomFromRepr(params)(paper.view.center);
+  // create a diatom from a representation
+  let c = document.createElement("canvas");
+  c.width = 100;
+  c.height = 100;
+  initCanvas(c, () => {
+    diatomFromRepr(params)(paper.view.center);
 
-		const fileName = "diatom.svg"
-		const url = "data:image/svg+xml;utf8," + encodeURIComponent(paper.project.exportSVG({asString:true}));
-		var link = document.createElement("a");
-		link.download = fileName;
-		link.href = url;
-		document.body.appendChild(link);
-		link.click();
-		// link.remove();
-		// console.log(link);
-	});
+    const fileName = "diatom.svg";
+    const url =
+      "data:image/svg+xml;utf8," +
+      encodeURIComponent(paper.project.exportSVG({ asString: true }));
+    var link = document.createElement("a");
+    link.download = fileName;
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
+    // link.remove();
+    // console.log(link);
+  });
 }
 
 function generateCanvases(num) {
-	for (let i=1; i <= num; i++) {
-		let c = document.createElement('canvas');
-		c.id = i;
-		c.title = `#${i}`;
-		document.getElementById('art-wall').append(c);
-		
-		const p = getRandomParams();
-		initCanvas(c, () => diatom(paper.view.center, p.iter, p.r1, p.r2));
+  for (let i = 1; i <= num; i++) {
+    let c = document.createElement("canvas");
+    c.id = i;
+    c.title = `#${i}`;
+    document.getElementById("art-wall").append(c);
 
-		// log and save diatom parameters
-		let repr = {
-			element: c,
-			params: p,
-			project: paper.project
-		};
-		console.debug(repr);
-		diatoms[i] = repr;
-	}
+    const p = getRandomParams();
+    initCanvas(c, () => diatom(paper.view.center, p.iter, p.r1, p.r2));
+
+    // log and save diatom parameters
+    let repr = {
+      element: c,
+      params: p,
+      project: paper.project
+    };
+    console.debug(repr);
+    diatoms[i] = repr;
+  }
 }
--- golden-spiral/script.js
+++ golden-spiral/script.js
@@ -8,27 +8,27 @@ PHI = (1 + Math.sqrt(5)) / 2;
 
 // return the dimensions of the next rectangle
 function getBabyRect(width, height) {
-    if (width > height) {
-        return {
-            width: width - height,
-            height: height
-        }
-    } else {
-        return {
-            width: width,
-            height: height - width
-        }
-    }
+  if (width > height) {
+    return {
+      width: width - height,
+      height: height
+    };
+  } else {
+    return {
+      width: width,
+      height: height - width
+    };
+  }
 }
 
 // visualize a rectangle object
 function viz(r, level) {
-    p = new Path.Rectangle(r);
-    p.strokeColor = 'black';
-    p.fillColor = {0: 'red', 1: 'green', 2: 'yellow', 3: 'blue'}[level % 4];
-    
-    length = Math.max(r.size.width, r.size.height);
-    p.insertBelow(l);  // keep curve on top
+  p = new Path.Rectangle(r);
+  p.strokeColor = "black";
+  p.fillColor = { 0: "red", 1: "green", 2: "yellow", 3: "blue" }[level % 4];
+
+  length = Math.max(r.size.width, r.size.height);
+  p.insertBelow(l); // keep curve on top
 }
 
 // TODO: draw complete curve
@@ -49,11 +49,11 @@ c = view.getViewSize();
 
 // phi is a/b, a > b
 // fit first rectangle to canvas
-if (c.width/c.height >= PHI) {
-    height = c.height - 20;
-    size = new Size(height*PHI, height);
+if (c.width / c.height >= PHI) {
+  height = c.height - 20;
+  size = new Size(height * PHI, height);
 } else {
-    size = new Size(c.width, c.width*PHI);
+  size = new Size(c.width, c.width * PHI);
 }
 
 r = new Rectangle(new Point(0, 0), size);
@@ -67,16 +67,22 @@ r.center = view.center;
 
 // begin spiral
 l = new Path();
-l.strokeColor = 'white';
-l.add(new Segment(
+l.strokeColor = "white";
+l.add(
+  new Segment(
     r.point + new Size(0, r.size.height),
     new Point(0, r.size.height / 2),
-    new Point(0, -r.size.height / 2)));
-l.add(new Segment(
+    new Point(0, -r.size.height / 2)
+  )
+);
+l.add(
+  new Segment(
     r.point + new Size(r.size.height, 0),
     new Point(-r.size.height / 2, 0),
     r.point + new Size(r.size.height, 0),
-    new Point(r.size.height / 2, 0)));
+    new Point(r.size.height / 2, 0)
+  )
+);
 // l.fullySelected = true;
 l.visible = false;
 
@@ -84,23 +90,23 @@ viz(r, 0);
 
 // draw the rest of the spiral
 parent = r;
-for (i=1; i<ITERATIONS; i++) {
-    size = new Size(getBabyRect(parent.width, parent.height));  // dimensions
-    // get coordinates of top left or rectangle
-    x = 0;
-    y = 0;
-    switch (i % 4) {
-        case 1:
-            x += parent.size.width - size.width;
-            break;
-        case 2:
-            y += parent.size.height - size.height;
-            break;
-    }
-    point = parent.point + new Size(x, y);
-    parent = new Rectangle(point, size);
-    console.debug('level '+i);
-    console.debug(parent);
+for (i = 1; i < ITERATIONS; i++) {
+  size = new Size(getBabyRect(parent.width, parent.height)); // dimensions
+  // get coordinates of top left or rectangle
+  x = 0;
+  y = 0;
+  switch (i % 4) {
+    case 1:
+      x += parent.size.width - size.width;
+      break;
+    case 2:
+      y += parent.size.height - size.height;
+      break;
+  }
+  point = parent.point + new Size(x, y);
+  parent = new Rectangle(point, size);
+  console.debug("level " + i);
+  console.debug(parent);
 
-    viz(parent, i);
+  viz(parent, i);
 }
